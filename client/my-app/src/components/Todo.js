6b59858f4d611e130021894f7291a981f5391f48
Santosh Prajapati <101245775+Prajapati-santosh@users.noreply.github.com>
2024-08-22 11:07:34 +0000

    test siute commit

---
+++ client/my-app/src/components/Todo.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Todo(){
+    return <div>
+        <input data-testid="test-1"/>  
+        <h1 data-testid="test-2">Hello world</h1>
+    </div>;
+}
+
+export default Todo;
